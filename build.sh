fa961733a3f3fd65277000598d466feb3fd15e7e
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2021-10-30 20:17:44 +0200

    Adjusted config and build script.

--- build.sh
+++ build.sh
@@ -23,20 +23,3 @@ pipenv run mypy wfleet/
 # todo: put output to separated file?
 pipenv run flake8 wfleet/
 pipenv run flake8 tests/
-
-# - install virtualenv
-#pip3 install virtualenv
-# - create virtual environment
-#virtualenv --verbose .venv
-# - activate virtual environment
-#source .venv/bin/activate
-# - upgrade pip3 in virtual environment
-#python3 -m pip3 install --upgrade pip
-# - install necessary dependencies in virtual environment (according to requirements)
-#pip3 install -r requirements.txt
-# - run unit tests with coverage and XML/HTML reports
-#python3 -m nose2 --verbose --start-dir fleet_scraper_tests --plugin nose2.plugins.junitxml \
-#    -X --with-coverage --coverage fleet_scraper \
-#    --coverage-report xml --junit-xml-path .coverage/nose2-junit.xml --coverage-report html
-# - deactivate virtual environment (exit)
-#deactivate
