95d4421ae708b620edf392fbf359ee87a280cc3e
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2022-01-11 14:47:22 +0100

    Minor changes, work in progress.

--- src/wfleet/scraper/engine/scraper_abstract.py
+++ src/wfleet/scraper/engine/scraper_abstract.py
@@ -4,7 +4,7 @@
     Scrapers Abstractions. Define base interface / behavior / properties for all Scrapers.
 
     Created:  Dmitrii Gusev, 02.05.2021
-    Modified: Dmitrii Gusev, 31.05.2021
+    Modified: Dmitrii Gusev, 09.01.2022
 """
 
 import logging
@@ -22,19 +22,16 @@ class ScraperAbstractClass(ABC):
     """Base Abstract Class for all scrapers. Define base behavior and properties for all scrapers."""
 
     def __init__(self):
-        """Base Constructor for scrapers. Define necessary fields.
-        :param source_name:
-        :param cache_path:
-        """
+        """Base Constructor for scrapers. Define necessary fields."""
         log.info("Scraper Abstract Class: initializing.")
 
     @abstractmethod
     def scrap(self, timestamp: datetime, dry_run: bool, requests_limit: int = 0) -> str:
         """Abstract method to be overridden by subclasses.
+        :param timestamp: scrap timestamp - use one timestamp for all scrapers.
         :param dry_run: dry run -> true/false, default = false (each run is real, not 'dry').
             If dry run = True, requests_limit parameter will be ignored.
         :param requests_limit: limit for performed HTTP requests to the source system, default = 0 (no limit).
             Any value <= 0 - no limit.
         :return: text message - scrap result
         """
-        pass
